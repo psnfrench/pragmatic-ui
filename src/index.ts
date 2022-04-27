d1b4b84cc43022b12604b35e917370c4c5c04414
Paul French <psnfrench@gmail.com>
2022-04-28 08:59:22 +1200

    change target and module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { theme } from './constants/theme';
 export * from './components/PTextField';
 export * from './constants/Colors';
-// export * from './constants/theme';
-
-export { theme };
+export * from './constants/theme';
