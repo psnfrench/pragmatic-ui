1edfd8f527f1a152202430d204033594adb770ec
Paul French <psnfrench@gmail.com>
2022-04-28 08:40:47 +1200

    test export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { theme } from './constants/theme';
 export * from './components/PTextField';
 export * from './constants/Colors';
-export * from './constants/theme';
+// export * from './constants/theme';
+
+export { theme };
