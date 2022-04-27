f967a09048564fa7e5a94d434022ccbae82f6132
Paul French <psnfrench@gmail.com>
2022-04-28 09:15:11 +1200

    change target and module

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,5 +1,4 @@
 // export const presets = ['@babel/preset-env', '@babel/preset-react'];
 module.exports = {
-  plugins: ['@babel/plugin-transform-modules-commonjs'],
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
 };
