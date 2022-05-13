347e226bd594adf5084128d719375bccebb5419d
Paul French <psnfrench@gmail.com>
2022-05-13 15:13:08 +1200

    export PRadioGroup

--- src/pragmatic-ui.ts
+++ src/pragmatic-ui.ts
@@ -2,5 +2,6 @@ export * from './components/ErrorLabel';
 export * from './components/PTextField';
 export * from './components/PDateTimePicker';
 export * from './components/PSwitch';
+export * from './components/PRadioGroup';
 export * from './constants/Colors';
 export * from './constants/theme';
