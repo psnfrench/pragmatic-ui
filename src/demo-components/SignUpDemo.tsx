5317d42313c5fef6cef7bcaa50e99f1a6367408c
Paul French <psnfrench@gmail.com>
2022-05-17 13:45:04 +1200

    sign in demo updated

--- src/demo-components/SignUpDemo.tsx
+++ src/demo-components/SignUpDemo.tsx
@@ -1,11 +1,37 @@
+import { Box, Divider, Typography, Grid } from '@mui/material';
 import React from 'react';
 import SignUpCard from '../components/SignUpCard';
+import { Colors } from '../constants/Colors';
+import LandscapeIcon from '@mui/icons-material/Landscape';
 
 function SignUpDemo() {
   const handleSubmit = () => {};
   const goToLogin = () => {};
 
-  return <SignUpCard onSubmit={handleSubmit} onGoToLogin={goToLogin} />;
+  return (
+    <Box>
+      <Divider />
+      <Typography variant="h4">Sign in page</Typography>
+      <Divider />
+      <Grid container>
+        <Grid item xs={6}>
+          <SignUpCard onSubmit={handleSubmit} onGoToLogin={goToLogin} />
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          sx={{
+            backgroundColor: Colors.greyscale.coolGrey400,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <LandscapeIcon sx={{ fontSize: 300 }} />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default SignUpDemo;
