eb35c1f339401802638e1eefce87b458a8a96f4d
Paul French <psnfrench@gmail.com>
2022-05-31 19:45:08 +1200

    export datepicker

--- src/pragmatic-ui.ts
+++ src/pragmatic-ui.ts
@@ -1,6 +1,7 @@
 export * from './components/ErrorLabel';
 export * from './components/PTextField';
 export * from './components/PDateTimePicker';
+export * from './components/PDatePicker';
 export * from './components/PSwitch';
 export * from './components/PRadioGroup';
 export * from './components/PasswordInput';
