3607ec130afb20c0aa7484684be794e04824f34f
Paul French <psnfrench@gmail.com>
2022-06-28 08:43:00 +1200

    Do not pass down error to textfield so that datepicker errors work correctly

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,6 @@
 import { Theme } from '@mui/material';
 import { Formik, FormikHelpers } from 'formik';
-import React, { useContext } from 'react';
+import React from 'react';
 import './App.css';
 import TextDemo from './demo-components/TextDemo';
 import RadioDemo from './demo-components/RadioDemo';
@@ -9,9 +9,8 @@ import SnackBarDemo from './demo-components/SnackBarDemo';
 import ConfirmationDemo from './demo-components/ConfirmationDemo';
 import BorderRadiusDemo from './demo-components/BorderRadiusDemo';
 import DateDemo from './demo-components/DateDemo';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import UserContext from './context/user';
 import LoginDemo from './demo-components/LoginDemo';
 
 const initialValues = {
@@ -24,46 +23,48 @@ const initialValues = {
 
 export type AppRouterProps = {
   setTheme: React.Dispatch<React.SetStateAction<Theme>>;
-}
+};
 
 export const AppRouter = ({ setTheme }: AppRouterProps) => {
-
-  const { isLoggedIn } = useContext(UserContext);
-  
   const handleSubmit = (values: typeof initialValues, formikHelpers: FormikHelpers<typeof initialValues>) => {
     console.log('values: ', values);
   };
 
+  const validate = (values: typeof initialValues) => {
+    const errors: Partial<{ [key in keyof typeof initialValues]: string }> = {};
+    (Object.keys(initialValues) as (keyof typeof initialValues)[]).forEach((k) => {
+      if (!values[k]) {
+        errors[k] = 'Required';
+      }
+    });
+    return errors;
+  };
+
   return (
-        <Routes>
-          
-          <Route path="/" element={<Home />} />
-              <Route
-                path="/border-radius"
-                element={ <BorderRadiusDemo setTheme={setTheme} />}
-              />
-              <Route
-                path="/demos"
-                element={
-                  <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-                    {({ handleSubmit }) => (
-                      <form onSubmit={handleSubmit}>
-                        <TextDemo />
-                        <DateDemo />
-                        <RadioDemo />
-                        <button type="submit">Submit</button>
-                      </form>
-                    )}
-                  </Formik>
-                }
-              />
-              <Route path="/login" element={<LoginDemo />} />
-              <Route path="/signup" element={<SignUpDemo />} />
-              <Route path="/snackbar" element={<SnackBarDemo />} />
-              <Route path="/confirmation" element={<ConfirmationDemo />} />
-         
-        </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/border-radius" element={<BorderRadiusDemo setTheme={setTheme} />} />
+      <Route
+        path="/demos"
+        element={
+          <Formik initialValues={initialValues} onSubmit={handleSubmit} validate={validate}>
+            {({ handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <TextDemo />
+                <DateDemo />
+                <RadioDemo />
+                <button type="submit">Submit</button>
+              </form>
+            )}
+          </Formik>
+        }
+      />
+      <Route path="/login" element={<LoginDemo />} />
+      <Route path="/signup" element={<SignUpDemo />} />
+      <Route path="/snackbar" element={<SnackBarDemo />} />
+      <Route path="/confirmation" element={<ConfirmationDemo />} />
+    </Routes>
   );
-}
+};
 
 export default AppRouter;
