f5928a2aa7a4c4d2fa21adb3ab51e6e6372fd0e6
Paul French <psnfrench@gmail.com>
2022-06-27 14:27:36 +1200

    export SignInCard

--- src/components/SignInCard.tsx
+++ src/components/SignInCard.tsx
@@ -29,7 +29,7 @@ const SignInCard = ({
   const validate = (values: LoginFormValues) => {
     // eslint-disable-next-line no-unused-vars
     const errors: Partial<{ [key in keyof LoginFormValues]: string }> = validateEmail(values);
-    (Object.keys(initialValues) as Array<keyof typeof initialValues>).forEach((k) => {
+    (Object.keys(initialValues) as (keyof typeof initialValues)[]).forEach((k) => {
       if (!values[k]) {
         errors[k] = 'Required';
       }
