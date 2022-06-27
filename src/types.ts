f1fc3b9f56b099ea1ef8dfcc002799acdddfd014
maxadamsdes <max-adams@live.nmit.ac.nz>
2022-06-27 12:28:12 +1200

    1.0.66

--- src/types.ts
+++ src/types.ts
@@ -1,40 +1,40 @@
 export type BaseFields = {
-    _id: string;
-    createdAt: number;
-    updatedAt: number;
-  };
-  
-  export type User = StaffInfo &
-    BaseFields & {
-      email?: string | null;
-      active: boolean;
-      password?: string; // for setting screen only
-      type?: 'admin' | 'staff';
-      avatarColor: string;
-    };
-  
-    export type StaffInfo = {
-      gender: string;
-      firstName: string;
-      lastName: string;
-      jobRole: string;
-      startingDate: number;
-      dateOfBirth: number;
-      phoneNumber: string;
-      email: string;
-      certificateNumber?: string;
-      certificateExpiryDate?: number;
-      residentialAddress?: string;
-      staffsReplace?: string[];
-      reasonReplace?: string;
-      documents?: FileInfo[];
-      files?: File[];
-    };
+  _id: string;
+  createdAt: number;
+  updatedAt: number;
+};
 
-    export type FileInfo = {
-        eTag: string;
-        locationUrl: string;
-        key: string;
-        origFileName: string;
-        timeUploaded: number;
-      };
+export type User = StaffInfo &
+  BaseFields & {
+    email?: string | null;
+    active: boolean;
+    password?: string; // for setting screen only
+    type?: 'admin' | 'staff';
+    avatarColor: string;
+  };
+
+export type StaffInfo = {
+  gender: string;
+  firstName: string;
+  lastName: string;
+  jobRole: string;
+  startingDate: number;
+  dateOfBirth: number;
+  phoneNumber: string;
+  email: string;
+  certificateNumber?: string;
+  certificateExpiryDate?: number;
+  residentialAddress?: string;
+  staffsReplace?: string[];
+  reasonReplace?: string;
+  documents?: FileInfo[];
+  files?: File[];
+};
+
+export type FileInfo = {
+  eTag: string;
+  locationUrl: string;
+  key: string;
+  origFileName: string;
+  timeUploaded: number;
+};
